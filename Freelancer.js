b513bafc9f75880f0c221227a3766ce0c23a78ac
Sjpandya0210 <Sjpandya1991@gmail.com>
2023-11-20 15:02:28 -0500

    tested average function

--- Freelancer.js
+++ Freelancer.js
@@ -7,7 +7,7 @@
     startingPrice: 50 }];
     */
 
-const nameOfFreelancer = ["Alice", "Bob", "Carol", "Jon"];
+const nameOfFreelancer = ["Alice", "Bob", "Carol", "Joe"];
 const occupations = ["Writer", "Teacher","Programmer", "Musician"];
 const startingPrice = [30, 50, 70, 30];
 const allFreelancers = [
@@ -22,12 +22,14 @@ const allFreelancers = [
         startingPrice: 50,
     },
 ];
+const MaxValue = nameOfFreelancer.length;
 const freelancersList = [];
 
-// `setInterval` will call `freelancers` every 2000 milliseconds (2 second)
- const freelancersIntervalId = setInterval(addfreelancers, 2000);
 
- render(); // We call this function once to render the initial state
+// `setInterval` will call `freelancers` every 2000 milliseconds (2 second)
+ const freelancersIntervalId = setInterval(addfreelancers, 1000);
+
+ //render(); // We call this function once to render the initial state
 /**
  * Update the DOM to reflect the current state.
  * The term "render" is often used to describe this process.
@@ -55,23 +57,41 @@ while (freelancersList.some((freelancer) => freelancer.name === randomName)) {
 }
 
     freelancersList.push({ name: randomName, occupation: randomOccupation, startingPrice: randomPrice });
-    render();
-    averagePrice(); // Call the averagePrice function
 
+    // Call the averagePrice function
     render();
+    averagePrice()
       // TODO: Stop adding shapes if we've reached the maximum number of shapes
-    if (freelancersList.length >= allFreelancers.length) {
+    if (freelancersList.length >= MaxValue) {
      clearInterval(freelancersIntervalId);
     }
 
 }
-function averagePrice (){
+function averagePrice() {
+    // Assuming countTotalPrice is defined somewhere in your code
+   let countTotalPrice = freelancersList.map((freelancer) => freelancer.startingPrice);
+
     let sum = 0;
-    for (let i = 0; i < startingPrice.length; i++ ){
-     sum += startingPrice[i];
+    for (let i = 0; i < countTotalPrice.length; i++) {
+        sum += countTotalPrice[i];
     }
-    const avg = sum / startingPrice.length;
+
+    const avg = sum / countTotalPrice.length;
     const averagePriceElement = document.querySelector("#averagePrice");
     averagePriceElement.innerText = `Average Price of All Freelancers: $${avg.toFixed(2)}`;
 }
+
+// Run the function after the document has fully loaded
+
+/*
+function averagePrice (){
+    let  = 0;
+    for (let i = 0; i < countTotalPrice.length; i++ ){
+     sum += countTotalPrice[i];
+    }
+    const avg = sum / countTotalPrice.length;
+    const averagePriceElement = document.querySelector("#averagePrice");
+    averagePriceElement.innerText = `Average Price of All Freelancers: $${avg.toFixed(2)}`;
+}
+averagePrice(); */
 // The result will displayed in the averagePriceElement with two decimal places 
